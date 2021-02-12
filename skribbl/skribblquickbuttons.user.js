56e86bd722d287747e38b5bc86cb037ebf6c9fcc
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-02-12 11:41:57 +0100

    Update skribblquickbuttons.user.js

--- skribbl/skribblquickbuttons.user.js
+++ skribbl/skribblquickbuttons.user.js
@@ -11,6 +11,7 @@
 
 (function() {
     'use strict';
+    let pickForMeIn = false;
     setTimeout(() => {
         var button = document.createElement("button");
         button.className = "btn btn-danger btn-block"
@@ -37,4 +38,18 @@
             document.getElementsByClassName("lobbyContentButtons")[0].appendChild(button4)
         }
     }, 500);
+    setInterval(() => {
+        if(document.getElementsByClassName("wordContainer")[0].children.length > 0) {
+            if(document.getElementsByClassName("wordContainer")[0].style.display == "" && !pickForMeIn) {
+                pickForMeIn = true
+                var button = document.createElement("div");
+                button.className = "word"
+                button.innerText = "Pick for me!"
+                button.setAttribute("onclick", "document.getElementsByClassName('word')[Math.floor(Math.random() * (2 - 0 + 1)) + 0].click()")
+                document.getElementsByClassName("wordContainer")[0].appendChild(button)
+            } else if (document.getElementsByClassName("wordContainer")[0].style.display == "none" && pickForMeIn) {
+                pickForMeIn = false
+            }
+        }
+    }, 1000);
 })();
