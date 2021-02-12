cadffb318bf278dd9b297772a197c57d73f622b6
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-02-12 09:12:42 +0100

    bugfixing

--- skribbl/skribblcrownsforall.user.js
+++ skribbl/skribblcrownsforall.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skribbl: Crowns for All
 // @namespace    https://vukky.ga
-// @version      0.3
+// @version      0.4
 // @description  Everyone gets a crown! This is JUST VISUAL and will NOT make you the owner. Incompatible with No Avatars.
 // @author       Vukky
 // @match        https://skribbl.io/**
@@ -11,13 +11,15 @@
 
 (function() {
     'use strict';
-    if(!window.vukkyNoAvatarsInstalled) {
-        setInterval(() => {
-            var crowns = document.getElementsByClassName("owner");
-            for (let i = 0; i < crowns.length; i++) {
-                const crown = crowns[i];
-                crown.style.display = "";
-            }
-        }, 1000); 
-    }
+    setTimeout(() => {
+        if(!window.vukkyNoAvatarsInstalled) {
+            setInterval(() => {
+                var crowns = document.getElementsByClassName("owner");
+                for (let i = 0; i < crowns.length; i++) {
+                    const crown = crowns[i];
+                    crown.style.display = "";
+                }
+            }, 1000); 
+        }
+    }, 500);
 })();
--- skribbl/skribblquickbuttons.user.js
+++ skribbl/skribblquickbuttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skribbl: Quick Buttons
 // @namespace    https://vukky.ga
-// @version      0.1
+// @version      0.2
 // @description  Adds quick buttons to the game.
 // @author       Vukky
 // @match        https://skribbl.io/**
@@ -11,28 +11,30 @@
 
 (function() {
     'use strict';
-    var button = document.createElement("button");
-    button.className = "btn btn-danger btn-block"
-    button.innerText = "Disconnect"
-    button.setAttribute("onclick", "document.location.href='https://skribbl.io'")
-    document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button)
-    if(window.vukkySkribblUrlShortcutsInstalled) {
-        var button2 = document.createElement("button");
-        button2.className = "btn btn-danger btn-block"
-        button2.innerText = "Switch servers"
-        button2.setAttribute("onclick", "document.location.href='https://skribbl.io/?play'")
-        document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button2)
-
-        var button3 = document.createElement("button");
-        button3.className = "btn btn-danger btn-block"
-        button3.innerText = "Start private game"
-        button3.setAttribute("onclick", "document.location.href='https://skribbl.io/?create'")
-        document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button3)
-
-        var button4 = document.createElement("button");
-        button4.className = "btn btn-danger"
-        button4.innerText = "Join Public Game"
-        button4.setAttribute("onclick", "document.location.href='https://skribbl.io/?play'")
-        document.getElementsByClassName("lobbyContentButtons")[0].appendChild(button4)
-    }
+    setTimeout(() => {
+        var button = document.createElement("button");
+        button.className = "btn btn-danger btn-block"
+        button.innerText = "Disconnect"
+        button.setAttribute("onclick", "document.location.href='https://skribbl.io'")
+        document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button)
+        if(window.vukkySkribblUrlShortcutsInstalled) {
+            var button2 = document.createElement("button");
+            button2.className = "btn btn-danger btn-block"
+            button2.innerText = "Switch servers"
+            button2.setAttribute("onclick", "document.location.href='https://skribbl.io/?play'")
+            document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button2)
+    
+            var button3 = document.createElement("button");
+            button3.className = "btn btn-danger btn-block"
+            button3.innerText = "Start private game"
+            button3.setAttribute("onclick", "document.location.href='https://skribbl.io/?create'")
+            document.getElementsByClassName("tooltip-wrapper")[0].appendChild(button3)
+    
+            var button4 = document.createElement("button");
+            button4.className = "btn btn-danger"
+            button4.innerText = "Join Public Game"
+            button4.setAttribute("onclick", "document.location.href='https://skribbl.io/?play'")
+            document.getElementsByClassName("lobbyContentButtons")[0].appendChild(button4)
+        }
+    }, 500);
 })();
