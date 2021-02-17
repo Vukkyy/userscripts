deb9c999df0614aba85d4a8596978e34dc3569ce
Vukky <46850780+VukAnd@users.noreply.github.com>
2021-02-17 22:40:31 +0100

    skribbl+

--- skribbl/README.md
+++ skribbl/README.md
@@ -1,7 +1,12 @@
 # Userscripts for skribbl.io
 Tested in Tampermonkey, on Microsoft Edge Chromium `90.0.794.0 (Official build) canary (64-bit)`.
 
-**NEW SCRIPTS:** skribblsurprise
+**NEW SCRIPTS:** skribbl+
+
+## skribbl+ [(Install)](https://github.com/Vukky123/userscripts/raw/main/skribbl/skribblplus.user.js)
+A experimental userscript that will combine all of my userscripts into one.
+
+The avatar customization scripts (crownsforall, noavatarbodies, noavatars, and a special new one also related to crowns 👀) are currently supported.
 
 ## skribblcrownsforall [(Install)](https://github.com/Vukky123/userscripts/raw/main/skribbl/skribblcrownsforall.user.js)
 Gives all avatars a crown. **Purely visual. You will not become an owner.**
--- skribbl/skribblplus.user.js
+++ skribbl/skribblplus.user.js
@@ -4,10 +4,8 @@
 // @version      0.1.0
 // @description  skribbl+ is a combination of all the Skribbl userscripts that I have previously created.
 // @author       Vukky
-// @match        https://skribbl.io/**
-// @grant        none
-// @updateURL    https://raw.githubusercontent.com/Vukky123/userscripts/main/skribbl/skribblnoavatarbodies.user.js
-
+// @match        http*://skribbl.io/*
+// @updateURL    https://raw.githubusercontent.com/Vukky123/userscripts/main/skribbl/skribblplus.user.js
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -16,23 +14,176 @@
 
 (function() {
     'use strict';
-    setTimeout(() => {
-        if(!window.vukkyNoAvatarsInstalled) {
-            setInterval(() => {
-                var bodies = document.getElementsByClassName("color");
-                for (let i = 0; i < bodies.length; i++) {
-                    const body = bodies[i];
-                    body.remove();
-                }
-        
-                var avatarArrows = document.getElementsByClassName("avatarArrow");
-                for (let i = 0; i < avatarArrows.length; i++) {
-                    const avatarArrow = avatarArrows[i];
-                    if(avatarArrow.className.startsWith("avatarArrow avatarArrow") && avatarArrow.attributes[1].value == "0") {
-                        avatarArrow.remove();
+    GM_registerMenuCommand("skribbl+: Settings", opencfg);
+    function opencfg() {
+        GM_config.open();
+    }
+    GM_config.init(
+        {
+          'id': 'skribblplus',
+          'title': "skribbl+",
+          'fields': // Fields object
+          {
+            'removeavatars':
+            {
+              'label': 'Remove avatars',
+              'section': ['Avatar Customization', 'Customize the Skribbl avatars.'], 
+              'type': 'checkbox',
+              'default': false
+            },
+            'crownsforall':
+            {
+              'label': "Give all avatars a crown (refresh to disable)",
+              'type': 'checkbox',
+              'default': false
+            },
+            'crownsfornone':
+            {
+              'label': "Remove crowns from avatars (refresh to disable)",
+              'type': 'checkbox',
+              'default': false
+            },
+            'removeavatarbodies':
+            {
+              'label': "Remove all bodies from avatars",
+              'type': 'checkbox',
+              'default': false
+            },
+          },
+          'css': "#skribblplus .section_header_holder { text-align: center; }",
+          'events': {
+            'open': function(doc) {
+                function removeAvatarsDisabler() {
+                    var crownsforall = GM_config.get('crownsforall', true);
+                    var crownsfornone = GM_config.get('crownsfornone', true);
+                    var removeavatarbodies = GM_config.get('removeavatarbodies', true);
+                    if(crownsforall == false && removeavatarbodies == false && crownsfornone == false) {
+                        GM_config.fields['removeavatars'].node.disabled = false
+                    } else {
+                        GM_config.fields['removeavatars'].node.disabled = true
                     }
                 }
-            }, 1000);
+                var crownsfornone = GM_config.get('crownsforall', true);
+                if(crownsfornone == false) {
+                    GM_config.fields['crownsforall'].node.disabled = false
+                } else {
+                    GM_config.fields['crownsforall'].node.disabled = true
+                }
+                removeAvatarsDisabler();
+                GM_config.fields['removeavatars'].node.addEventListener('change', function () {
+                    var removeavatars = GM_config.get('removeavatars', true);
+                    if(removeavatars == false) {
+                        GM_config.fields['crownsforall'].node.disabled = false
+                        GM_config.fields['crownsfornone'].node.disabled = false
+                        GM_config.fields['removeavatarbodies'].node.disabled = false
+                    } else {
+                        GM_config.fields['crownsforall'].node.disabled = true
+                        GM_config.fields['crownsfornone'].node.disabled = true
+                        GM_config.fields['removeavatarbodies'].node.disabled = true
+                    }
+                }, false);
+                GM_config.fields['crownsforall'].node.addEventListener('change', function () {
+                    var crownsforall = GM_config.get('crownsforall', true);
+                    removeAvatarsDisabler();
+                    if(crownsforall == false) {
+                        GM_config.fields['crownsfornone'].node.disabled = false
+                    } else {
+                        GM_config.fields['crownsfornone'].node.disabled = true
+                    }
+                }, false);
+                GM_config.fields['crownsfornone'].node.addEventListener('change', function () {
+                    var crownsfornone = GM_config.get('crownsfornone', true);
+                    removeAvatarsDisabler();
+                    if(crownsfornone == false) {
+                        GM_config.fields['crownsforall'].node.disabled = false
+                    } else {
+                        GM_config.fields['crownsforall'].node.disabled = true
+                    }
+                }, false);
+                GM_config.fields['removeavatarbodies'].node.addEventListener('change', function () {
+                    var crownsforall = GM_config.get('crownsforall', true);
+                    var removeavatarbodies = GM_config.get('removeavatarbodies', true);
+                    if(crownsforall == false && removeavatarbodies == false) {
+                        GM_config.fields['removeavatars'].node.disabled = false
+                    } else {
+                        GM_config.fields['removeavatars'].node.disabled = true
+                    }
+                }, false);
+            }
+          }
+    });
+
+    setInterval(() => {
+        if(GM_config.get('removeavatars') == true) {
+            var avatars = document.getElementsByClassName("avatar");
+            for (let i = 0; i < avatars.length; i++) {
+                const avatar = avatars[i];
+                avatar.style.display = 'none';
+            }
+            var avatarContainers = document.getElementsByClassName("avatarContainer");
+            for (let i = 0; i < avatarContainers.length; i++) {
+                const avatarContainer = avatarContainers[i];
+                avatarContainer.style.display = 'none';
+            }
+            document.getElementById("loginAvatarCustomizeContainer").style.display = 'none';
+            document.getElementById("logoAvatarContainer").style.display = 'none';
+        } else if (GM_config.get('removeavatars') == false) {
+            var avatars = document.getElementsByClassName("avatar");
+            for (let i = 0; i < avatars.length; i++) {
+                const avatar = avatars[i];
+                if(avatar.id == "logoAvatarDummy") continue;
+                avatar.style.display = '';
+            }
+            var avatarContainers = document.getElementsByClassName("avatarContainer");
+            for (let i = 0; i < avatarContainers.length; i++) {
+                const avatarContainer = avatarContainers[i];
+                avatarContainer.style.display = '';
+            }
+            document.getElementById("loginAvatarCustomizeContainer").style.display = '';
+            document.getElementById("logoAvatarContainer").style.display = '';
+        }
+        if(GM_config.get('crownsforall') == true) {
+            var crowns = document.getElementsByClassName("owner");
+            for (let i = 0; i < crowns.length; i++) {
+                const crown = crowns[i];
+                crown.style.display = "";
+            }
+        }
+        if(GM_config.get('crownsfornone') == true) {
+            var crowns = document.getElementsByClassName("owner");
+            for (let i = 0; i < crowns.length; i++) {
+                const crown = crowns[i];
+                crown.style.display = "none";
+            }
+        }
+        if(GM_config.get('removeavatarbodies') == true) {
+            var bodies = document.getElementsByClassName("color");
+            for (let i = 0; i < bodies.length; i++) {
+                const body = bodies[i];
+                body.style.display = "none";
+            }
+    
+            var avatarArrows = document.getElementsByClassName("avatarArrow");
+            for (let i = 0; i < avatarArrows.length; i++) {
+                const avatarArrow = avatarArrows[i];
+                if(avatarArrow.className.startsWith("avatarArrow avatarArrow") && avatarArrow.attributes[1].value == "0") {
+                    avatarArrow.style.display = "none";
+                }
+            }
+        } else if (GM_config.get('removeavatarbodies') == false) {
+            var bodies = document.getElementsByClassName("color");
+            for (let i = 0; i < bodies.length; i++) {
+                const body = bodies[i];
+                body.style.display = "";
+            }
+    
+            var avatarArrows = document.getElementsByClassName("avatarArrow");
+            for (let i = 0; i < avatarArrows.length; i++) {
+                const avatarArrow = avatarArrows[i];
+                if(avatarArrow.className.startsWith("avatarArrow avatarArrow") && avatarArrow.attributes[1].value == "0") {
+                    avatarArrow.style.display = "";
+                }
+            }
         }
     }, 500);
 })();
