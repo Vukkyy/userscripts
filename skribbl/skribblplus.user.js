bb903dca5cbad14117deaa6c5ce1444158f08db5
Vukky <46850780+VukAnd@users.noreply.github.com>
2021-02-18 10:52:52 +0100

    Update skribblplus.user.js

--- skribbl/skribblplus.user.js
+++ skribbl/skribblplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         skribbl+
 // @namespace    https://vukky.ga
-// @version      0.1.0
+// @version      0.2.0
 // @description  skribbl+ is a combination of all the Skribbl userscripts that I have previously created.
 // @author       Vukky
 // @match        http*://skribbl.io/*
@@ -22,12 +22,12 @@
         {
           'id': 'skribblplus',
           'title': "skribbl+",
-          'fields': // Fields object
+          'fields':
           {
             'removeavatars':
             {
               'label': 'Remove avatars',
-              'section': ['Avatar Customization', 'Customize the Skribbl avatars.'], 
+              'section': ['Avatar Customization', 'Customize the Skribbl avatars.'],
               'type': 'checkbox',
               'default': false
             },
@@ -46,6 +46,25 @@
             'removeavatarbodies':
             {
               'label': "Remove all bodies from avatars",
+              'type': 'checkbox',
+              'default': false
+            },
+            'deletechatmessages':
+            {
+              'label': 'Delete individual chat messages',
+              'section': ['Misc', 'Various other features.'],
+              'type': 'checkbox',
+              'default': false
+            },
+            'removechat':
+            {
+              'label': "Remove the chat (but why?)",
+              'type': 'checkbox',
+              'default': false
+            },
+            'urlshortcuts':
+            {
+              'label': "URL shortcuts (skribbl.io/?play, skribbl.io/?create)",
               'type': 'checkbox',
               'default': false
             },
@@ -109,6 +128,22 @@
                         GM_config.fields['removeavatars'].node.disabled = true
                     }
                 }, false);
+                GM_config.fields['removechat'].node.addEventListener('change', function () {
+                    var removechat = GM_config.get('removechat', true);
+                    if(removechat == false) {
+                        GM_config.fields['deletechatmessages'].node.disabled = false
+                    } else {
+                        GM_config.fields['deletechatmessages'].node.disabled = true
+                    }
+                }, false);
+                GM_config.fields['deletechatmessages'].node.addEventListener('change', function () {
+                    var deletechatmessages = GM_config.get('deletechatmessages', true);
+                    if(deletechatmessages == false) {
+                        GM_config.fields['removechat'].node.disabled = false
+                    } else {
+                        GM_config.fields['removechat'].node.disabled = true
+                    }
+                }, false);
             }
           }
     });
@@ -142,6 +177,7 @@
             document.getElementById("loginAvatarCustomizeContainer").style.display = '';
             document.getElementById("logoAvatarContainer").style.display = '';
         }
+
         if(GM_config.get('crownsforall') == true) {
             var crowns = document.getElementsByClassName("owner");
             for (let i = 0; i < crowns.length; i++) {
@@ -149,6 +185,7 @@
                 crown.style.display = "";
             }
         }
+
         if(GM_config.get('crownsfornone') == true) {
             var crowns = document.getElementsByClassName("owner");
             for (let i = 0; i < crowns.length; i++) {
@@ -156,13 +193,14 @@
                 crown.style.display = "none";
             }
         }
+
         if(GM_config.get('removeavatarbodies') == true) {
             var bodies = document.getElementsByClassName("color");
             for (let i = 0; i < bodies.length; i++) {
                 const body = bodies[i];
                 body.style.display = "none";
             }
-    
+
             var avatarArrows = document.getElementsByClassName("avatarArrow");
             for (let i = 0; i < avatarArrows.length; i++) {
                 const avatarArrow = avatarArrows[i];
@@ -176,7 +214,7 @@
                 const body = bodies[i];
                 body.style.display = "";
             }
-    
+
             var avatarArrows = document.getElementsByClassName("avatarArrow");
             for (let i = 0; i < avatarArrows.length; i++) {
                 const avatarArrow = avatarArrows[i];
@@ -185,5 +223,47 @@
                 }
             }
         }
-    }, 500);
+
+        if(GM_config.get('deletechatmessages') == true) {
+            // Some of this code is from https://github.com/Sv443/skribbl.io-plus.
+            // Copyright (c) 2018 Sv443 / Sven Fehler
+            var deleteButton = "<span id='del_msg' style='font-weight: bold; color: red; cursor: pointer;' title='Delete this message'>X</span> ";
+            var messages = document.getElementById("boxMessages").childNodes;
+            for (let i = 0; i < messages.length; i++) {
+                const message = messages[i].innerHTML;
+                if(message == undefined) break;
+                if(messages[i].querySelector("#del_msg") == null) {
+                    messages[i].innerHTML = deleteButton + message
+                    messages[i].querySelector("#del_msg").addEventListener("click", function(){this.parentNode.remove();});
+                }
+            }
+        } else if (GM_config.get('deletechatmessages') == false) {
+            var messages = document.getElementById("boxMessages").childNodes;
+            for (let i = 0; i < messages.length; i++) {
+                if(messages[i].querySelector("#del_msg") != null) {
+                    messages[i].querySelector("#del_msg").remove();
+                }
+            }
+        }
+        
+        if(GM_config.get('urlshortcuts') == true) {
+            if(!document.getElementById("screenLogin").style.display != "none") {
+                if(location.search == "?play") {
+                    document.getElementById("formLogin").getElementsByTagName("button")[0].click();
+                    document.getElementById("containerLogoBig").style.display = ""
+                } else if (location.search == "?create") {
+                    document.getElementById("formLogin").getElementsByTagName("button")[1].click();
+                    document.getElementById("containerLogoBig").style.display = ""
+                }
+            }
+        }
+
+        if(GM_config.get('removechat') == true) {
+            document.getElementById("boxMessages").style.display = "none";
+        } else if (GM_config.get('removechat') == false) {
+            if(document.getElementById("screenGame").style.display == "") {
+                document.getElementById("boxMessages").style.display = "";
+            }
+        }
+    }, 100);
 })();
