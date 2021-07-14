b3ea97cb9088f54b6d3912d34faa9e7de52a960f
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-07-14 21:23:26 +0200

    adding support for Gamepedia dark mode and making some improvements

--- fandomdesktopdestroyer.user.js
+++ fandomdesktopdestroyer.user.js
@@ -1,23 +1,35 @@
 // ==UserScript==
 // @name         FandomDesktop Destroyer
 // @namespace    https://vukky.ga
-// @version      0.1.0
+// @version      0.2.0
 // @description  No one likes you and your attempts to ruin everything, Fandom.
 // @author       Vukky
 // @match        https://*.fandom.com/wiki/**
 // @icon         https://www.google.com/s2/favicons?domain=fandom.com
 // @updateURL    https://raw.githubusercontent.com/Vukky123/userscripts/main/fandomdesktopdestroyer.user.js
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    GM_registerMenuCommand('Toggle Gamepedia dark mode', async () => {
+        await GM_setValue("dark", !await GM_getValue("dark", false));
+        if(!document.body.classList.contains("is-gamepedia")) alert(`Just to let you know, this isn't a Gamepedia wiki. Your choice has been saved - Gamepedia dark mode is now turned ${await GM_getValue("dark", false) == true ? "on" : "off"}. Press OK to finish.`);
+        undesktop(await GM_getValue("dark", false));
+    })
+
     if(document.body.classList.contains("skin-fandomdesktop")) {
+        undesktop(GM_getValue("dark", false));
+    }
+
+    function undesktop(dark) {
         let noskin = new URL(document.location.href);
-        if(document.querySelector(".gp-wiki-badge")) {
+        if(document.body.classList.contains("is-gamepedia")) {
             // gamepedia wikis use a different skin, oasis doesn't work here
-            noskin.searchParams.set("useskin", "hydra");
+            noskin.searchParams.set("useskin", `hydra${dark == true ? "dark" : ""}`);
         } else {
             noskin.searchParams.set("useskin", "oasis");
         }
