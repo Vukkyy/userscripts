4993659a5aae29c8636887a82bcfffe2b42002aa
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-09-10 12:59:41 +0200

    Update vastaisaladinator.user.js

--- vastaisaladinator.user.js
+++ vastaisaladinator.user.js
@@ -1,17 +1,31 @@
 // ==UserScript==
 // @name         Vast.ai Saladinator
 // @namespace    https://vukky.ga
-// @version      0.3.3
+// @version      0.4.0
 // @description  Improves vast.ai for usage with salad.com.
 // @author       Vukky
 // @match        https://vast.ai/console/**
 // @icon         https://www.google.com/s2/favicons?domain=vast.ai
 // @updateURL    https://raw.githubusercontent.com/Vukkyy/userscripts/main/vastaisaladinator.user.js
 // @grant        GM_registerMenuCommand
+// @grant        GM_xmlhttpRequest
+// @connect      app-api.salad.io
 // ==/UserScript==
 
+var saladRateExpiry = "Loading...";
 (function() {
     'use strict';
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://app-api.salad.io/api/v2/bonuses/earning-rate",
+        onload: function(response) {
+          if(response.status == 401) {
+            saladRateExpiry = "<a href='https://app.salad.io/login' target='_blank'>Login with Salad</a>";
+          }
+          saladRateExpiry = new Date(JSON.parse(response.responseText).endsAt).toLocaleString();
+        }
+    });
 
     if(!localStorage.getItem("ozuaindex")) {
         localStorage.setItem("ozuaindex", "200");
@@ -82,7 +96,7 @@
             if(!document.querySelector("#fancystats")) {
                 let fancyStats = document.createElement("div");
                 fancyStats.id = "fancystats";
-                fancyStats.innerHTML = "<div id='averageOZI'></div><div id='instancePrice'></div>"
+                fancyStats.innerHTML = "<div id='averageOZI'></div><div id='instancePrice'></div><div id='earningRateExpiry'></div><br>"
                 document.querySelector(".instances-table").insertBefore(fancyStats, document.querySelector(".card-expando"));
             }
             let ozuaIndexes = Array.from(document.querySelectorAll(".dlperf")).map(e => parseInt(e.innerHTML.split(" ")[0]));
@@ -98,7 +112,8 @@
                 totalGpuPrice += gpuPrices[i];
             }
             document.querySelector("#averageOZI").innerHTML = `Average <a onclick="alert('The Ozua Index is a way to determine if a machine is profitable, using the formula (gpu hashrate * gpu amount / price). The higher, the better.')">Ozua Index</a>: ${parseInt(totalOzuaIndex / ozuaIndexes.length)}`
-            document.querySelector("#instancePrice").innerHTML = `Total cost of active instances: $${totalGpuPrice.toFixed(3)}/hr ($${totalGpuPrice.toFixed(3) / 2 / 4}/15 min to be profitable)`
+            document.querySelector("#instancePrice").innerHTML = `Total cost of active instances: $${totalGpuPrice.toFixed(3)}/hr ($${(totalGpuPrice.toFixed(3) / 2 / 4).toFixed(3)}/15min to be profitable)`
+            document.querySelector("#earningRateExpiry").innerHTML = `Salad earning rate will expire on: ${saladRateExpiry}`
         }
     }, 1000);
 })();
