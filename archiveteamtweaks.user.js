62893b50baf40b19c87331870f796d76b95bb035
Vukky <vukky@litdevs.org>
2023-07-13 20:26:13 +0200

    Update archiveteamtweaks.user.js

--- archiveteamtweaks.user.js
+++ archiveteamtweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ArchiveTeam Tweaks
 // @namespace    https://vukky.ga
-// @version      0.6.8
+// @version      0.6.9
 // @description  Tweakings ArchiveTeam
 // @author       Vukky
 // @match        http*://tracker.archiveteam.org/**
